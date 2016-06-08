be561cdf552ab983042bab1da2569983f4016c1f
root <LianyuMa@users.noreply.github.com>
2016-06-07 22:04:56 -0400

    remove arrow callback style in test file

--- test/index-test.js
+++ test/index-test.js
@@ -1,16 +1,16 @@
 const app = require('../index');
 const request = require('supertest').agent(app.listen());
 
-describe('Server', () => {
-  describe('when GET /', () => {
-    it('should say `Koalaborate`', (done) => {
+describe('Server', function () {
+  describe('when GET /', function () {
+    it('should say `Koalaborate`', function (done) {
       request
         .get('/')
         .expect(200)
         .expect('Koalaborate', done);
     });
 
-    it('should set X-Response-Time', (done) => {
+    it('should set X-Response-Time', function (done) {
       request
         .get('/')
         .expect('X-Response-Time', /ms$/)
